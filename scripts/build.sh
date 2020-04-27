89b9103813699a28974b357522160e942593e99e
Jagendra Atal Prakash <japrakash@Jagendras-MBP.broadband>
2020-04-27 19:00:02 +0100

    Corrected gcp var

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,16 +5,11 @@ set -ex
 # project name same as file name for onboarding
 readonly PROJNAME="kubernetes-cp"
 
-cat <<EOF > creds.json
-$GCP_CREDS
-EOF
-
-export GOOGLE_APPLICATION_CREDENTIALS="creds.json"
+export GOOGLE_APPLICATION_CREDENTIALS=$GCP_CREDS
 
 cd terraform
 terraform init
 terraform plan
 terraform apply -auto-approve
-rm creds.json
 
 
