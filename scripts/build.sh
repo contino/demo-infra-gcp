dfa1c3dee03a3d55b8e596980a3de75fb9edf133
Jagendra Atal Prakash <japrakash@Jagendras-MBP.broadband>
2020-04-27 20:14:45 +0100

    Reverted changes

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,20 @@
 
 set -ex
 
-export GOOGLE_APPLICATION_CREDENTIALS="${GCP_CREDS}"
+# project name same as file name for onboarding
+readonly PROJNAME="kubernetes-cp"
+
+cat <<EOF > creds.json
+$GCP_CREDS
+EOF
+
+export GOOGLE_APPLICATION_CREDENTIALS="creds.json"
 
 cd terraform
 terraform init
 terraform plan
 terraform apply -auto-approve
+rm creds.json
+
 
 
