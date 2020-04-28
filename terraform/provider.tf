eb37b1a92273db8b620336f236cdbb23a77c68a5
Jagendra Atal Prakash <jagendraatal.prakash@contino.io>
2020-04-28 15:37:55 +0100

    Another fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,11 @@
 provider "google" {
+  credentials = "account.json"
   project     = "kubernetes-cp"
   region      = "europe-west1"
 }
 
 provider "google-beta" {
+  credentials = "account.json"
   project     = "kubernetes-cp"
   region      = "europe-west1"
 }
