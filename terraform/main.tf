a9934bbacc0ca015a779f71e469fa4c7b1033b58
Jagendra Atal Prakash <jagendraatal.prakash@contino.io>
2020-04-29 10:36:43 +0100

    GKE version update

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ module "cluster_1" {
   pod_subnet_range                    = "10.224.0.0/17"
   service_subnet_range                = "10.128.0.0/17"
   nodepool_count                      = 1
-  min_master_version                  = "1.14.10-gke.24"
+  min_master_version                  = "1.14.10-gke.27"
   machine_type                        = "n1-standard-1"
   default_min_nodes_per_zone_per_pool = var.default_min_nodes_per_zone_per_pool
   default_max_nodes_per_zone_per_pool = var.default_max_nodes_per_zone_per_pool
