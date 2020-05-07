77c65d4fc9fc767c5df94b7545cc5226789c4d63
Jagendra Atal Prakash <jagendraatal.prakash@contino.io>
2020-05-07 20:56:58 +0100

    kuberenetes secret creation issue, fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,3 @@
-locals {
-  kubernetes = {
-    host                   = "https://${module.cluster_1.cluster_endpoint}"
-    config_context_cluster = module.cluster_1.cluster_name
-    cluster_ca_certificate = base64decode(module.cluster_1.cluster_ca_certificate)
-    token                  = data.google_client_config.current.access_token
-  }
-}
-
 provider "google" {
   version = "~> 3.18.0"
   project = var.project_id
@@ -18,5 +9,9 @@ provider "google-beta" {
 }
 
 provider "kubernetes" {
-  version = "~> 1.11"
+  host                   = "https://${module.cluster_1.cluster_endpoint}"
+  cluster_ca_certificate = base64decode(module.cluster_1.cluster_ca_certificate)
+  token                  = data.google_client_config.current.access_token
+  load_config_file       = false
+  version                = "~> 1.11"
 }
