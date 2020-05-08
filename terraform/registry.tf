673e7a65911f9171d2c07fcf372d78d813a668fd
Jagendra Atal Prakash <jagendraatal.prakash@contino.io>
2020-05-08 12:55:32 +0100

    Revert "Secret data correction"
    This reverts commit 900439d389291a4da8d625564fbf6a200db85272.

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -42,7 +42,7 @@ resource "kubernetes_secret" "image-puller" {
   data = {
     ".dockerconfigjson" = jsonencode({
       "auths" : {
-        "eu.gcr.io" : {
+        "https://gcr.io" : {
           email    = google_service_account.image-puller.email
           username = "_json_key"
           password = trimspace(base64decode(google_service_account_key.image-puller.private_key))
