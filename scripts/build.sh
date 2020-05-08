f83cc01548652c7de5f50b6ed3ffaf88b62f61ae
Jagendra Atal Prakash <jagendraatal.prakash@contino.io>
2020-05-08 12:49:16 +0100

    Trying to fix some issue with bash in CircleCI

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 . ./scripts/functions.sh
 
-assert_variables_set "$GCP_CREDS" "$CLUSTER_NAME" "$CLUSTER_REGION" "$GCP_PROJECT"
+assert_variables_set "${GCP_CREDS}" "${CLUSTER_NAME}" "${CLUSTER_REGION}" "${GCP_PROJECT}"
 
-authenticate_with_gcloud "$GCP_CREDS"
+authenticate_with_gcloud "${GCP_CREDS}"
 
 run_terraform "${GCP_CREDS}" "${GCP_PROJECT}"
 
