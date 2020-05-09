2e23401a35a7883e3fed3983fe9f5de24dd67aeb
Jagendra Atal Prakash <jagendraatal.prakash@contino.io>
2020-05-09 13:39:41 +0100

    Added static ip address

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,29 +1,32 @@
-output "cluster_1_name" {
+output "cluster_name" {
   value = module.cluster.cluster_name
 }
 
-output "cluster_1_region" {
+output "cluster_region" {
   value = module.cluster.cluster_region
 }
 
-output "cluster_1_endpoint" {
+output "cluster_endpoint" {
   value = module.cluster.cluster_endpoint
 }
 
-output "cluster_1_service_account_email" {
+output "cluster_service_account_email" {
   value = module.cluster.service_account_email
 }
 
-output "cluster_1_cluster_ca_certificate" {
+output "cluster_cluster_ca_certificate" {
   value     = module.cluster.cluster_ca_certificate
   sensitive = true
 }
 
-output "cluster_1_subnet_name" {
+output "cluster_subnet_name" {
   value = module.cluster.subnet_name
 }
 
-output "cluster_1_network_name" {
+output "cluster_network_name" {
   value = module.cluster.network_name
 }
 
+output "cluster_static_ip" {
+  value = google_compute_address.static.address
+}
